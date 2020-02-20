87cad20b2e0070e0d4e00f7407e898c642bf338e
Olechka2018 <58590796+Olechka2018@users.noreply.github.com>
2020-02-20 20:03:22 +0700

    asdfh

--- Django_blog/apps/my_blog/admin.py
+++ Django_blog/apps/my_blog/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from Django_blog.apps.my_blog.models import Post
 from Django_blog.apps.my_blog.models import Key_word
+from Django_blog.apps.my_blog.models import Author
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['post_title']
+    list_filter = ['id']
 
 
-admin.site.register(Post, admin.ModelAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author, admin.ModelAdmin)
 admin.site.register(Key_word, admin.ModelAdmin)
 
--- Django_blog/apps/my_blog/models.py
+++ Django_blog/apps/my_blog/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+Male = 'Male'
+Female = 'Female'
+choises = {(Male, 'Male'),
+            (Female, 'Female')}
+
 
 class Key_word(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -9,6 +14,17 @@ class Key_word(models.Model):
         return f'{self.id} {self.key_word}'
 
 
+class Author(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=150)
+    birth_date = models.DateField()
+    is_active = models.BooleanField(default=False)
+    ava = models.ImageField(upload_to="static/images", blank=True)
+    email = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100, choices=choises, default=Male)
+
+
 class Post(models.Model):
     id = models.BigAutoField(primary_key=True)
     post_title = models.CharField(max_length=100)
@@ -16,8 +32,10 @@ class Post(models.Model):
     post_text = models.TextField()
     key_words = models.ManyToManyField(Key_word, blank=True)
     post_image = models.ImageField(upload_to="static/images", verbose_name='photo', blank=True)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
         return f'{self.id} {self.post_title}'
+
 
 
